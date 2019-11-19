fddb2f88b1a7d7cb9ad1e9756d0356722ba68ed1
holgerengels <hengels@gmail.com>
2019-11-19 20:34:54 +0100

    o open-wc update

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,20 +1,39 @@
 import cpy from 'rollup-plugin-cpy';
-import createDefaultConfig from '@open-wc/building-rollup/modern-config';
+import createDefaultConfig from '@open-wc/building-rollup';
+import deepmerge from 'deepmerge';
+const { injectManifest, generateSW } = require('rollup-plugin-workbox');
 
-const config = createDefaultConfig({ input: './index.html' });
+// if you need to support IE11 use "modern-and-legacy-config" instead.
+// import { createCompatibilityConfig } from '@open-wc/building-rollup';
+// export default createCompatibilityConfig({ input: './index.html' });
 
-export default [
-  {
-    ...config,
-    plugins: [
-      ...config.plugins,
-      cpy([
-        { files: 'favicon.ico', dest: 'dist' },
-        { files: '*.png', dest: 'dist' },
-        { files: 'manifest.json', dest: 'dist' },
-        { files: 'browserconfig.xml', dest: 'dist' },
-        { files: 'geogebra.html', dest: 'dist' },
-      ]),
-    ],
-  }
-];
+const config = createDefaultConfig({
+  input: './index.html',
+  plugins: {
+    workbox: false,
+  },
+});
+
+export default deepmerge(config, {
+  plugins: [
+    generateSW({
+      swDest: 'dist/sw.js',
+      globDirectory: 'dist/',
+      navigateFallback: '/',
+    }),
+    /*
+    injectManifest({
+      swSrc: 'dist/sw.js',
+      swDest: 'dist/sw.js',
+      globDirectory: 'dist/',
+    }),
+     */
+    cpy([
+      { files: 'favicon.ico', dest: 'dist' },
+      { files: '*.png', dest: 'dist' },
+      { files: 'manifest.json', dest: 'dist' },
+      { files: 'browserconfig.xml', dest: 'dist' },
+      { files: 'geogebra.html', dest: 'dist' },
+    ]),
+  ],
+});
