68df4a43179915e966b2f532ab55781927cab42d
holgerengels <hengels@gmail.com>
2020-03-02 09:58:04 +0100

    o load polyfills on demand

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -25,7 +25,7 @@ export default deepmerge(config, {
         customPolyfills: [
           {
             name: 'event-target',
-            test: "!('EventTarget' in window) || !('constructor' in window.ResizeObserver.prototype)",
+            test: "!('EventTarget' in window) || !('constructor' in window.EventTarget.prototype)",
             path: require.resolve('event-target/min.js'),
           },
           {
