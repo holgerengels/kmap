b5e2c7fe97c5cccaa45426c01f5c3038ae2708fd
holgerengels <hengels@gmail.com>
2020-03-26 10:31:47 +0100

    o get rid of nextcloud, store files in couchdb

--- client/src/models/index.ts
+++ client/src/models/index.ts
@@ -9,7 +9,6 @@ export { default as courses } from './courses';
 export { default as contentMaps } from './contentMaps';
 export { default as contentSets } from './contentSets';
 export { default as tests } from './tests';
-export { default as cloud } from './cloud';
 export { default as uploads } from './uploads';
 export { default as testUploads } from './testUploads';
 export { default as feedback } from './feedback';
