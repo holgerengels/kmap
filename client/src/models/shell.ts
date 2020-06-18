cd689ad27ff8f1f1abf4999f68ecb374cf98b353
holgerengels <hengels@gmail.com>
2020-06-18 14:34:19 +0200

    improvement(usability): code splitting for faster initial load

--- client/src/models/shell.ts
+++ client/src/models/shell.ts
@@ -60,11 +60,36 @@ export default createModel({
       window.setTimeout(() => dispatch.shell.removeMessage(payload), 3000);
       dispatch.shell.addMessage(payload);
     },
+    async addLayer(layer: string) {
+      if (layer === 'editor') {
+        await import('../components/kmap-module-selector');
+        await import('../components/kmap-editor-add-fabs');
+        await import('../components/kmap-editor-delete-dialog');
+        await import('../components/kmap-editor-edit-dialog');
+        await import('../components/kmap-editor-rename-dialog');
+
+        await import('../components/kmap-set-selector');
+        await import('../components/kmap-test-editor-add-fabs');
+        await import('../components/kmap-test-editor-delete-dialog');
+        await import('../components/kmap-test-editor-edit-dialog');
+      }
+      else if (layer === 'averages') {
+        await import('../components/kmap-course-selector');
+      }
+    },
 
     'routing/change': async function(routing: RoutingState) {
       const dispatch = store.dispatch();
       if (routing.page !== 'browser')
         dispatch.shell.updateMeta({});
+      if (routing.page === 'courses') {
+        console.log("loading courses")
+        import('../components/kmap-courses').then(() => console.log("loaded courses"));
+      }
+      else if (routing.page === 'content-manager') {
+        console.log("loading content-manager")
+        import('../components/kmap-content-manager').then(() => console.log("loaded content-manager"));
+      }
     },
 
     'app/receivedLogout': async function() {
