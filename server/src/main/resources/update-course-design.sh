bded59bb98f942af34660848ae8f8ecb9f215f93
holgerengels <hengels@gmail.com>
2020-08-22 14:02:59 +0200

    chore(timeline): curriculum editor and selector

--- server/src/main/resources/update-course-design.sh
+++ server/src/main/resources/update-course-design.sh
@@ -1,4 +1,4 @@
-REV=`curl -X GET -u $AUTH http://localhost:5984/$INST-course/_design/net | jq ._rev | tr -d \"`
+REV=`curl -X GET -u $AUTH http://localhost:5984/$INST-course/_design/course | jq ._rev | tr -d \"`
 echo $REV
-curl -X DELETE -u $AUTH http://localhost:5984/$INST-course/_design/net?rev=$REV
-curl -X PUT    -u $AUTH http://localhost:5984/$INST-course/_design/net -d @design-course.json
+curl -X DELETE -u $AUTH http://localhost:5984/$INST-course/_design/course?rev=$REV
+curl -X PUT    -u $AUTH http://localhost:5984/$INST-course/_design/course -d @design-course.json
