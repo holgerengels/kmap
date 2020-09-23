cc9b0086fefe23f3c2dfcac3d5e4f7b90093ef8b
holgerengels <hengels@gmail.com>
2020-09-23 10:02:51 +0200

    fix(security): samesite cookie

--- client/src/urls.ts
+++ client/src/urls.ts
@@ -1,6 +1,6 @@
 export const urls = {
-  server: "http://127.0.0.1:8081/server/",
-  client: "http://127.0.0.1:8080/app/",
+  server: "/server/",
+  client: "/app/",
 };
 
 export const encode = (...path: string[]): string => {
