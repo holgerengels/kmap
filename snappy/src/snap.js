4f9dabd3d53cfa962eb84d76eef5e65b5d60dcb5
holgerengels <hengels@gmail.com>
2021-01-18 21:25:47 +0100

    chore(browser): fix deep links

--- snappy/src/snap.js
+++ snappy/src/snap.js
@@ -11,7 +11,7 @@ let server;
 let browser;
 
 (async () => {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({args: ['--disable-dev-shm-usage'] });
 
     const requestListener = async function (req, res) {
         try {
