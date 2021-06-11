709c7ef46748f565e065f2781aba12185899d5ed
holgerengels <hengels@gmail.com>
2021-06-11 21:39:21 +0200

    chore(seo): educationalContext, educationalLevel and typicalAgeRange

---
+++ server/src/other/batch/add-meta.sh
@@ -0,0 +1,29 @@
+./add-property.sh educationalLevel "'7,8,9,10'" Grundwissen.json
+./add-property.sh typicalAgeRange "'12-15'" Grundwissen.json
+./add-property.sh educationalContext "'Sekundarstufe I'" Grundwissen.json
+./add-property.sh sgs 38001 Grundwissen.json
+
+./add-property.sh educationalLevel "'7,8,9,10,11'" Eingangsklasse.json
+./add-property.sh typicalAgeRange "'15-16'" Eingangsklasse.json
+./add-property.sh educationalContext "'Sekundarstufe I, Sekundarstufe II'" Eingangsklasse.json
+./add-property.sh sgs 38002 Eingangsklasse.json
+
+./add-property.sh educationalLevel "'10,11,12'" Differentialrechnung.json
+./add-property.sh typicalAgeRange "'15-17'" Differentialrechnung.json
+./add-property.sh educationalContext "'Sekundarstufe I, Sekundarstufe II'" Differentialrechnung.json
+./add-property.sh sgs 38002 Differentialrechnung.json
+
+./add-property.sh educationalLevel "'11,12,13'" Integralrechnung.json
+./add-property.sh typicalAgeRange "'16-18'" Integralrechnung.json
+./add-property.sh educationalContext "'Sekundarstufe II'" Integralrechnung.json
+./add-property.sh sgs 38002 Integralrechnung.json
+
+./add-property.sh educationalLevel "'10,11,12'" Stochastik.json
+./add-property.sh typicalAgeRange "'15-17'" Stochastik.json
+./add-property.sh educationalContext "'Sekundarstufe I, Sekundarstufe II'" Stochastik.json
+./add-property.sh sgs 38004 Stochastik.json
+
+./add-property.sh educationalLevel "'10,11,12,13'" Vektorgeometrie.json
+./add-property.sh typicalAgeRange "'15-18'" Vektorgeometrie.json
+./add-property.sh educationalContext "'Sekundarstufe I, Sekundarstufe II'" Vektorgeometrie.json
+./add-property.sh sgs 38002 Vektorgeometrie.json
