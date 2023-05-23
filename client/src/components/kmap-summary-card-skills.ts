0d84fe2031ff55ea9a4e82adfd64b8b357b300a1
holgerengels <hengels@gmail.com>
2023-05-23 16:09:12 +0200

    chore(skills): eye candy

--- client/src/components/kmap-summary-card-skills.ts
+++ client/src/components/kmap-summary-card-skills.ts
@@ -22,7 +22,7 @@ export class KMapSummaryCardSkills extends LitElement {
           padding: 16px;
         }
         abbr {
-          padding: 0px 8px 0px 6px;
+          padding: 0px 6px 0px 6px;
           margin: 0px 0px 3px 0px;
           border-radius: 4px;
           background-color: var(--color-primary);
